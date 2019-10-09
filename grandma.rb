e89fb469258df9cf2673262c852fcd2b754c1a7c
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-09 20:59:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
   if string == string.uppercase
-    return 
+    return ""
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
